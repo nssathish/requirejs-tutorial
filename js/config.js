8dff3ec04dab0f5f8fb8b87023054f241446f49d
U-ATHENAHEALTH\ssathishkumar <ssathishkumar@SSATHISHK-T450.corp.athenahealth.com>
2016-10-12 19:32:14 +0530

    Why am i doing this?? anyhow need to learn why i am adding the untracked files

---
+++ js/config.js
@@ -0,0 +1,12 @@
+// config.js
+
+requirejs.config ({
+	baseUrl: 'js',
+	paths: {
+		anglular: 'anglular.min',
+		extCore: 'ext-core',
+		jquery: 'jquery.min',
+		mootools: 'mootools.min',
+		swfobj: 'swfobject'
+	}
+});
