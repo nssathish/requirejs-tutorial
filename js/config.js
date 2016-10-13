f025e1e119f42ea6393d26f45aa10c172d67f89b
Sathish Kumar <neosathishgct@gmail.com>
2016-10-13 19:25:47 +0530

    Submitting the changse after chapter 5

--- js/config.js
+++ js/config.js
@@ -3,10 +3,13 @@
 requirejs.config ({
 	baseUrl: 'js',
 	paths: {
-		anglular: 'anglular.min',
-		extCore: 'ext-core',
-		jquery: 'jquery.min',
-		mootools: 'mootools.min',
+		angular: [
+			'https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min', //this is something we see that we can include the CDNs.
+			'angular.min',
+		],
+		extCore: 'ext-bootstrap',
+		jquery: 'jquery-3.1.1.min',
+		mootools: 'MooTools-Core-1.6.0',
 		swfobj: 'swfobject'
 	}
 });
---
+++ js/customscripts/jqueryfile.js
@@ -0,0 +1,6 @@
+//jQuery stuff
+define (['jquery','angular'],function($) {
+	$('body').html('Hello there from the other side!!')
+	//since we have included the 'angular' from the js/config.js file
+	//we can use it here too
+});
