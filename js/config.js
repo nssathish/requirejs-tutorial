338fabfa1fb5f81ec031b334a6529521823a414a
Sathish Kumar <neosathishgct@gmail.com>
2016-10-17 20:42:12 +0530

    Moved/modified whatever will happen in a workshop good to go

--- js/config.js
+++ js/config.js
@@ -5,11 +5,12 @@ requirejs.config ({
 	paths: {
 		angular: [
 			'https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min', //this is something we see that we can include the CDNs.
-			'angular.min',
+			'lib/angular.min',
 		],
-		extCore: 'ext-bootstrap',
-		jquery: 'jquery-3.1.1.min',
-		mootools: 'MooTools-Core-1.6.0',
-		swfobj: 'swfobject'
+		extCore: 'lib/ext-bootstrap',
+		jquery: 'lib/jquery-3.1.1.min',
+		mootools: 'lib/MooTools-Core-1.6.0',
+		swfobj: 'lib/swfobject',
+		methods: 'customscripts/methods'
 	}
 });
--- js/customscripts/jqueryfile.js
+++ js/customscripts/jqueryfile.js
@@ -1,6 +1,13 @@
 //jQuery stuff
-define (['jquery','angular'],function($) {
-	$('body').html('Hello there from the other side!!')
+//define (['jquery','methods'],function($,methods) {
+define (['jquery','methods'],function($,whatever) {
+	//$('body').html('Hello there from the other side!!')
 	//since we have included the 'angular' from the js/config.js file
 	//we can use it here too
+	
+	//using the 'methods' passed value here as part of the AMD
+	$('#clickMe').click(function () {
+		//methods.showAlert('I was clicked!!');
+		whatever.changeHTML('I was clicked!!');
+	});
 });
