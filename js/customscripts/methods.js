ec1fbe37ad418d003e05e8a4562d174808a92513
Sathish Kumar <neosathishgct@gmail.com>
2016-10-17 20:40:49 +0530

    Require js crash course completes with AMD at the end. AMD is vast that what it thought it is.

---
+++ js/customscripts/methods.js
@@ -0,0 +1,10 @@
+define (['jquery'], function ($) {
+	var methods = {};
+	methods.changeHTML = function (arg) {
+		$('body').html(arg);
+	};
+	methods.showAlert = function (arg) {
+		alert(arg);
+	};
+	return methods;
+});
